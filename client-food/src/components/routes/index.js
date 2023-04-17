e4d3e69c79a43441dbf62d2dddbd0f67336383ef
Thach2001 <vovanthach2001@gmail.com>
2023-04-17 20:59:23 +0700

    Xay dung layout

---
+++ client-food/src/components/routes/index.js
@@ -0,0 +1,20 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Admin from '~/pages/Admin';
+
+// Public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/admin', component: Admin, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
